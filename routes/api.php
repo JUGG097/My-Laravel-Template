774e0fece4edb4db34394abb8767d88037710e6e
Adeoluwa Adeboye <adeneye97@yahoo.com>
2023-06-29 14:17:39 +0100

    :construction: Added server health api route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/check', function (Request $request) {
+    return Response::json(["success" => true], 200);
 });
